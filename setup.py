82c12041bf740dd7ea3835d1f77d50b853d3b471
jcora <cora.1@osu.edu>
2015-06-24 14:45:56 -0400

    Updated release version in setup

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
 	name = 'oj_break',
 	packages = ['oj_break'],
-	version = '0.1',
+	version = '0.1.1',
 	description = 'A Python wrapper for uses the OJ_Break API',
 	author = 'Joe Cora',
 	author_email = 'cora.1@osu.edu',
 	url = 'https://github.com/JoeCora/OJ_Break-python',
-	download_url = 'https://github.com/JoeCora/OJ_Break-python/tarball/0.1',
+	download_url = 'https://github.com/JoeCora/OJ_Break-python/tarball/0.1.1',
 	keywords = ['xBio:D', 'OJ_Break', 'api', 'biodiversity', 'informatics', 'taxonomy', 'species'],
 	classifiers = [],
 )
