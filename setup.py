ef64e1bb33f0ee9631d2e313a8f699805639f8e8
jcora <cora.1@osu.edu>
2015-06-24 11:47:05 -0400

    Updated setup to conform to actual Python package name; removed classifiers (doesn't seem to matter)

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-	name = 'ojbreak-python',
-	packages = ['ojbreak-python'],
+	name = 'oj_break',
+	packages = ['oj_break'],
 	version = '0.1',
 	description = 'A Python wrapper for uses the OJ_Break API',
 	author = 'Joe Cora',
 	author_email = 'cora.1@osu.edu',
 	url = 'https://github.com/JoeCora/OJ_Break-python',
 	download_url = 'https://github.com/JoeCora/OJ_Break-python/tarball/0.1',
-	keywords = ['xBio:D', 'biodiversity', 'informatics', 'taxonomy', 'species'],
-	classifiers = ['Development Status :: 5 - Production/Stable', 'Programming Language :: Python :: 2.7', 'Topic :: Scientific/Engineering :: Bio-Informatics'],
+	keywords = ['xBio:D', 'OJ_Break', 'api', 'biodiversity', 'informatics', 'taxonomy', 'species'],
+	classifiers = [],
 )
